ec66c7120b82334452bd3a4d6e7791875bbacff2
Ray <ray@thelondonvandal.com>
2016-03-03 19:49:54 +0000

    added addressing mode switches

--- core.c
+++ core.c
@@ -21,19 +21,49 @@ int main(int argc, char *argv) {
    core[0].opcode = MOV;
    core[0].op1 = 2;
    core[0].op2 = 0;
+   core[0].op1_addr_mode = DIRE;
+   core[0].op2_addr_mode = DIRE;
+
+   uint16_t op1_addr = 0;
+   uint16_t op2_addr = 0;
 
    while(1) {
 
      for (i=0;i<799;i++) {
+       // resolve locations
+       switch((*(core_p+i)).op1_addr_mode) {
+         case IMMI:
+         break;
+         case DIRE:
+           op1_addr = (*(core_p+i)).op1_addr;
+         break;
+         case INDI:
+         break;
+         case PRED:
+         break;
+       }
+
+       switch((*(core_p+i)).op2_addr_mode) {
+         case IMMI:
+         break;
+         case DIRE:
+           op2_addr = (*(core_p+i)).op2_addr;
+         break;
+         case INDI:
+         break;
+         case PRED:
+         break;
+       }
+
        switch ((*(core_p+i)).opcode) {
+         default:
+           printf("Unrecognized opcode\r\n");
+         break;
          case ADD:
          break;
          case MOV:
-           printf("MOV %d, %d\n", (*(core_p+i)).op1, (*(core_p+i)).op2);
+           printf("MOV %d, %d\r\n", (*(core_p+i)).op1, (*(core_p+i)).op2);
            *((core_p+i)+(*(core_p)).op1) = *((core_p+i)+(*(core_p)).op2);
-         break;
-         default:
-           printf("Unrecognized 
          break;
        }
      }
